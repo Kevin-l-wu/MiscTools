48b025abf42fc8b7bdb09600086f0cd18e1a03f1
Kevin <0630wl@163.com>
2022-08-21 05:55:04 +0800

    Add flash download tool of STM32 through openocd

---
+++ FlashLoad.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+OPENOCD_PATH=/usr/local/Cellar/open-ocd/0.11.0/share/openocd
+INTERFACE=stlink-v2.cfg
+TARGET=stm32f4x.cfg
+
+#echo "\$#=$#"
+
+# check
+if [ $# -ne 1 ];then
+    echo "usage: FlashLoad.sh <.bin>"
+    exit 1
+fi
+
+# download
+openocd -f $OPENOCD_PATH/scripts/interface/$INTERFACE -f $OPENOCD_PATH/scripts/target/$TARGET -c init -c "reset halt" -c "flash write_image erase $1 0x08000000" -c shutdown
+
