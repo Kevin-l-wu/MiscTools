2afc2a33e359729b0b50f5f695d96b7422225185
Kevin <0630wl@163.com>
2022-08-24 17:10:29 +0800

    Add Multple Disks and Multple Partitions support

--- ReMount.sh
+++ ReMount.sh
@@ -1,56 +1,88 @@
 #!/bin/bash
 
+# for divided as '\n'
+IFS=$'\n\n'
 
-mount_dir=${HOME}/Desktop/Test
+disk_list=$(diskutil list)
+
+mount_dir_index=0
 
 
-# Invalid input
-if [ $# -ge 1 ]
+# Do remount core function 
+DoRemount(){
+	device_name=$1
+
+	mount_dir_index=$2
+
+	mount_dir=${HOME}/Desktop/Test"$mount_dir_index"
+
+	echo "$mount_dir"
+
+	# Create mount dir in desktop
+	if [ ! -e ${mount_dir} ]
+	then
+		mkdir -p "$mount_dir"
+		echo "$mount_dir"" has created"
+	else
+		echo "$mount_dir"" exist"
+	fi
+
+	# Unmount device 
+	sudo umount /dev/${device_name}
+
+
+	# Mounut device
+	echo "remounting..."
+
+	sudo mount -t ntfs -o rw,nobrowse /dev/${device_name} ${mount_dir}
+
+	#echo "\$?=$?"
+
+	if [ $? -eq 0 ]
+	then 	
+		echo "Remount success!"
+	else
+		echo "Remount failed!"
+	fi
+}
+
+# Find out all external disks
+disk_name_list=$(echo "$disk_list" | grep "external, physical")
+
+
+if [ "$disk_name_list" = "" ]
 then
-	echo "Useage: ./ReMount"
+	echo "No external device found!!!"
 	exit 1
 fi
 
+for disk_name in $disk_name_list
+do
+	# Get external disk name, such as:disk2
+	disk_name=$(expr "$disk_name" : '.*\(disk.\).*')
+	
+	# Get all describe string of a disk
+	disk_str=$(expr "$disk_list" : '.*\(/dev/'"$disk_name"'.*'"${disk_name}"'s.\).*$')
 
-# Get device name automatically
-TempStr=$(diskutil list | grep "Windows_NTFS")
+	
+	# Get all partition of a disk
+	partition_list=$(echo "$disk_str" | grep "Windows_NTFS")
+		
+		
+	for partition_name in {$partition_list}
+	do	
+		# Get partition identifier, such as: disk2s1
+		partition_name=$(expr "$partition_name" : '.*\('"$disk_name"'s.\).*')
+		
+		
+		if [ "$partition_name" != "" ]
+		then
 
-device_name=${TempStr:0-7}
+			mount_dir_index=$((mount_dir_index+1))
 
-if [ -z $device_name ]
-then
-	echo "No NTFS Disk found!!!"
-	exit 1
-fi
+			DoRemount "$partition_name" "$mount_dir_index"
 
-
-echo "\$device_name = $device_name"
-
-
-# Create mount dir in desktop
-if [ ! -e ${mount_dir} ]
-then
-	mkdir ${HOME}/Desktop/Test
-	echo "${HOME}/Desktop/Test has created"
-else
-	echo "${HOME}/Desktop/Test exist"
-fi
-
-# Unmount device 
-sudo umount /dev/${device_name}
-
-
-# Mounut device
-echo "remounting..."
-
-sudo mount -t ntfs -o rw,nobrowse /dev/${device_name} ${mount_dir}
-
-#echo "\$?=$?"
-
-if [ $? -eq 0 ]
-then 	
-	echo "Remount success!"
-else
-	echo "Remount failed!"
-fi
+		fi			
+	done
+done
 
